b60048e9f2ffeb39d0403de2e458f2d3a61d0e50
djcadaniel <djcadaniel@gmail.com>
2025-03-03 13:02:50 -0500

    add colors

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,6 +1,8 @@
 import { Form, useFetcher, useNavigate } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
+import { CiEdit } from "react-icons/ci"
+import { MdDelete } from "react-icons/md"
 
 type ProductsDetailProps = {
   product: Product
@@ -14,10 +16,10 @@ export const ProductDetails = ({product}: ProductsDetailProps) => {
 
   return (
      <tr className="border-b ">
-        <td className="p-3 text-lg text-gray-800">
+        <td className="p-3 text-md md:text-lg text-gray-800">
           {product.name}
         </td>
-        <td className="p-3 text-lg text-gray-800">
+        <td className="p-3 text-md md:text-lg text-gray-800">
           {formatCurrency(product.price)}
         </td>
         <td className="p-3 text-lg text-gray-800">
@@ -26,22 +28,22 @@ export const ProductDetails = ({product}: ProductsDetailProps) => {
               type="submit"
               name="id"
               value={product.id}
-              className={`${isAvailable ? 'text-black' : 'text-red-500'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
+              className={`${isAvailable ? ' bg-green-400 text-white' : 'bg-red-500 text-white'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
             >
               { isAvailable ? 'Disponible' : 'No disponible' }
             </button>
           </fetcher.Form>
         </td>
         <td className="p-3 text-lg text-gray-800 ">
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-5 items-center justify-center">
             <button
               onClick={()=> navigate(`/productos/${product.id}/editar`)}
-              className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center cursor-pointer"
+              className="bg-indigo-600 text-white rounded-lg p-1 uppercase font-bold text-xs text-center cursor-pointer"
             >
-              Editar
+              <CiEdit className="text-lg"/>
             </button>
             <Form
-              className="w-full"
+              className="flex items-center justify-center"
               method="POST"
               action={`productos/${product.id}/eliminar`}
               onSubmit={(e)=>{
@@ -50,11 +52,12 @@ export const ProductDetails = ({product}: ProductsDetailProps) => {
                 }
               }}
             >
-              <input 
+              <button 
                 type="submit" 
-                value="eliminar"
-                className="bg-red-600 text-white rounded-lg w-full p-2 uppercase fotn-bold text-xs text-center cursor-pointer"
-              />
+                className="bg-red-600 text-white rounded-lg p-1 uppercase font-bold text-xs text-center cursor-pointer flex items-center justify-center gap-2"
+              >
+                <MdDelete className="text-lg"/>
+              </button>
             </Form>
           </div>
         </td>
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -14,30 +14,30 @@ export default function Products() {
 
   return (
     <>
-      <div className="flex justify-between">
-        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mb-6">
+        <h2 className="text-xl sm:text-4xl font-black text-slate-700">Productos</h2>
         <Link 
           to='productos/nuevo'
-          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+          className="w-full sm:w-auto rounded-md bg-[#5499c7] p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500 text-center"
         >
           Agregar producto
         </Link>
       </div>
-      <div className="p-2">
-        <table className="w-full mt-5 table-auto">
-          <thead className="bg-slate-800 text-white">
+      <div className="overflow-x-auto rounded-lg shadow">
+        <table className="w-full mt-5 table-auto min-w-full divide-y divide-gray-200">
+          <thead className="bg-[#FCC71B] text-slate-700 text-md sm:text-lg">
               <tr>
-                  <th className="p-2">Producto</th>
-                  <th className="p-2">Precio</th>
-                  <th className="p-2">Disponibilidad</th>
-                  <th className="p-2">Acciones</th>
+                  <th className="p-2 whitespace-nowrap">Producto</th>
+                  <th className="p-2 whitespace-nowrap">Precio</th>
+                  <th className="p-2 whitespace-nowrap">Disponibilidad</th>
+                  <th className="p-2 whitespace-nowrap">Acciones</th>
               </tr>
           </thead>
-          <tbody>
+          <tbody className="divide-y divide-gray-200 bg-white">
             {products?.map(product => (
               <ProductDetails
-                key = {product.id}
-                product = {product}
+                key={product.id}
+                product={product}
               />
             ))}
           </tbody>
